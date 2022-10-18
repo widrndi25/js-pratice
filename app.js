d4825694f04212de2738c909ec0f46b4bab2582d
Lee JeongHyo <widrndi2@naver.com>
2022-10-18 20:59:32 +0900

    login

--- app.js
+++
@@ -1,9 +0,0 @@
-const loginInput = document.querySelector("#login-form input");
-const loginForm = document.querySelector("#login-form input");
-
-function onSubmit() {
-  const username = loginInput.value;
-  console.log(username);
-}
-
-loginForm.addEventListener("submit", onSubmit);
